a3dc3b30693120135fcf0778663310c17d1a2824
Trương Nhật Vy <truongnhatvy78@gmail.com>
2022-12-28 00:34:10 +0700

    [New Feature] Mini-Max Sum - Validate of input (5 positive integer) - Calculate minimum sum and the maximum sum of 4 of 5 elements.

---
+++ Algorithm/Utils.js
@@ -0,0 +1,43 @@
+export const isHaveFiveElements = (input) => {
+  const whiteSpace = input.split(" ").length - 1;
+
+  if (whiteSpace == 4) return true;
+
+  return false;
+};
+export const isPositiveInteger = (number) => {
+  const num = Number(number);
+
+  if (Number.isInteger(num) && num >= 0) {
+    return true;
+  }
+
+  return false;
+};
+export const quickSort = (arr) => {
+  if (arr.length < 2) return arr;
+
+  //lấy phần tử cuối của 'arr' làm 'pivot'
+  const pivotIndex = arr.length - 1;
+  const pivot = arr[pivotIndex];
+
+  const left = [];
+  const right = [];
+
+  let currentItem;
+  //i < pivotIndex' => chúng ta sẽ không loop qua 'pivot' nữa
+  for (let i = 0; i < pivotIndex; i++) {
+    currentItem = arr[i];
+
+    if (currentItem < pivot) {
+      left.push(currentItem);
+    } else {
+      right.push(currentItem);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+};
+export const clearValueInput = (id) =>{
+    document.getElementById(id).value = '';
+}
---
+++ Algorithm/index.js
@@ -0,0 +1,53 @@
+import {
+  isHaveFiveElements,
+  isPositiveInteger,
+  quickSort,
+  clearValueInput,
+} from "./Utils.js";
+
+export const validateInput = (input) => {
+  let arrInput = [];
+  let isHaveNotInteger = false;
+
+  if (isHaveFiveElements(input)) {
+    //chuyển đổi các phần tử từ string sang number
+    arrInput = input.split(" ").map((element) => +element);
+    arrInput.forEach((element) => {
+      if (!isPositiveInteger(element)) {
+        isHaveNotInteger = true;
+      }
+    });
+
+    if (isHaveNotInteger) {
+      alert("Vui lòng chỉ nhập số nguyên dương !");
+      clearValueInput("result");
+    } else {
+      miniMaxSum(arrInput);
+    }
+  } else {
+    alert("Vui lòng chỉ nhập 5 phần tử !");
+    clearValueInput("result");
+  }
+};
+
+export const miniMaxSum = (arr) => {
+  let arrAsc = [];
+  let minimum = 0;
+  let maximum = 0;
+
+  arrAsc = quickSort(arr);
+
+  // lấy 4 phần tử bé nhất đầu mảng
+  for (let i = 0; i < 4; i++) {
+    minimum += arrAsc[i];
+  }
+  //lấy 4 phần tử lớn nhất cuối mảng
+  for (let i = arrAsc.length - 1; i >= 1; i--) {
+    maximum += arrAsc[i];
+  }
+
+  document.getElementById("result").value = minimum + " " + maximum;
+};
+
+window.validateInput = validateInput;
+window.miniMaxSum = miniMaxSum;
